2d69892694f3fe61b825b0d37c88ddbd34261b9b
MMPhoze <olius-pop@yandex.ru>
2014-12-18 11:19:15 +0700

    отмена изменений

--- src/Metamorphoze/Core/Traits/Loader.php
+++ src/Metamorphoze/Core/Traits/Loader.php
@@ -2,8 +2,6 @@
 
 namespace Metamorphoze\Core\Traits;
 
-use Metamorphoze\Helpers\Core;
-
 trait Loader {
 
 	//=========================================================================
@@ -61,7 +59,7 @@ trait Loader {
 				foreach ( $class as $key => $value ) {
 
 					if ( is_class_name( $key ) ) {
-						$this->load( $value , $key);
+						$this->load( $value, $key );
 					}
 					else {
 						$this->load( $value );
@@ -78,27 +76,42 @@ trait Loader {
 
 	private function &load_class( $class, $alias )
 	{
+
 		if ( $alias === NULL ) {
-			$name = prepare_namespace( $class );
+			$name = $this->prepare_namespace( $class );
 		}
 		else {
-			$name = prepare_namespace( $alias );
+			$name = $this->prepare_namespace( $alias );
 		}
-		
+
 		return $this->{$name} = new $class();
-		
 	}
 
 	private function &load_object( &$class, $alias )
 	{
+
 		if ( $alias === NULL ) {
-			$name = prepare_namespace( get_class( $class ) );
+			$name = $this->prepare_namespace( get_class( $class ) );
 		}
 		else {
-			$name = prepare_namespace( $alias );
+			$name = $this->prepare_namespace( $alias );
 		}
-		
+
 		return $this->{$name} = $class;
+	}
+
+	/**
+	 * Подготавливает пространство имен для возможности использования
+	 * в качестве имени переменной.
+	 * 
+	 * @param type $namespace
+	 * @return type
+	 */
+	private function prepare_namespace( $namespace )
+	{
+		$from = array( 'metamorphoze\\', '\\' );
+		$to = array( '', '_' );
+		return str_replace( $from, $to, strtolower( $namespace ) );
 	}
 
 }
